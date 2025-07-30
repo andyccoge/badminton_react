5ab4f0eede522c8047a40346721b189686f29666
andyccoge <andyccoge@gmail.com>
2025-07-30 17:34:51 +0800

    tailwind版本調整+初始布局

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+/* npm run dev */ 
+/* npx tailwindcss -i ./src/assets/tailwind.css -o ./src/assets/tailwind_output.css --watch */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+    }
   },
-  plugins: [],
+  plugins: [
+    // require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),  /* <input type="email" class="form-input px-1 py-1 rounded" /> */
+    require('@tailwindcss/line-clamp')  /* <p class="line-clamp-3 md:line-clamp-none"></p> */
+    // require('@tailwindcss/aspect-ratio')
+  ],
 }
